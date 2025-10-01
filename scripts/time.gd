9c901973c79cde208eaaec1b50bb25d90eaf21f2
Nova <kt.drakon@gmail.com>
2025-10-01 08:35:52 -0400

    Version 2.1.2
    Functional Clock

--- scripts/time.gd
+++ scripts/time.gd
@@ -3,27 +3,42 @@ extends Label
 var hour = 1
 var minute = 0
 var second = 0
+var hoursPast = 13
+
+var timeNum = "0:00"
+var timeLabel = "X"
 
 func _process(_delta) -> void:
 	hourChange()
 	
 	if minute < 10:
-		self.text = str(hour) + ":0" + str(minute) + " PM"
+		timeNum = str(hour) + ":0" + str(minute)
 	else:
-		self.text = str(hour) + ":" + str(minute) + " PM"
+		timeNum = str(hour) + ":" + str(minute)
+	
+	if hoursPast >= 0 && hoursPast < 12:
+		timeLabel = "AM"
+	elif hoursPast >= 12 && hoursPast < 24:
+		timeLabel = "PM"
+	
+	self.text = timeNum + " " + timeLabel
 
 func hourChange():
 	minuteChange()
+	
+	if hoursPast == 25:
+		hoursPast = 0
 	
 	if hour == 13:
 		hour = 1
 	elif minute == 60:
 		hour += 1
+		hoursPast += 1
 	
 func minuteChange():
 	secondChange()
 	
-	if minute == 61:
+	if minute == 60:
 		minute = 0
 	elif second == 600:
 		minute += 1
