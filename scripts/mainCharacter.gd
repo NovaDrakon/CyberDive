5e10fc0efc1b9e02b112cb00dad5378c72cebc16
Nova <kt.drakon@gmail.com>
2025-11-09 20:13:36 -0500

    Version 2.3.2
    Main Character

--- scripts/mainCharacter.gd
+++ scripts/mainCharacter.gd
@@ -1,29 +1,27 @@
 extends CharacterBody2D
 
-@onready var bottomHalf: AnimatedSprite2D = $bottomHalf
-@onready var topHalf: AnimatedSprite2D = $topHalf
+@onready var characterSprite: AnimatedSprite2D = $characterSprite
 
 var speed = 250
-var jump = -300
+var jump = -350
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("Left", "Right")
 	
-	if direction < 0:
-		bottomHalf.play("leftIdle")
-		topHalf.play("leftIdle")
-	elif direction > 0:
-		bottomHalf.play("rightIdle")
-		topHalf.play("rightIdle")
+	if direction > 0:
+		characterSprite.flip_h = false
+	elif  direction < 0:
+		characterSprite.flip_h = true
 	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		characterSprite.play("Jump")
 		velocity.y = jump
+		await get_tree().create_timer(0.5).timeout
+		characterSprite.play("Idle")
 		
-	if Input.is_action_just_pressed("Dash") and is_on_floor():
-		velocity.x = direction * speed * 1.5
 	
 	if direction:
 		velocity.x = direction * speed
