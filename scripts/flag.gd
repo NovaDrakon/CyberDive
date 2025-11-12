d0229db636c2c9ac5b3e916cd99d925439ab970f
Nova <kt.drakon@gmail.com>
2025-11-12 12:19:54 -0500

    Version 2.3.6
    Debug Level, End Flag, & Cursor

--- scripts/flag.gd
+++ scripts/flag.gd
@@ -1,4 +1,6 @@
 extends Area2D
 
 func _on_body_entered(_body: Node2D) -> void:
+	await get_tree().create_timer(0.3).timeout
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	get_tree().change_scene_to_file("res://scenes/loadingScreen.tscn")
