f651973dba6247845005a392e3c632af9dba8c37
Nova <kt.drakon@gmail.com>
2026-02-02 09:37:18 -0500

    Version 3.1.1
    Camera Limits & Level Set-Up

--- scripts/folder.gd
+++ scripts/folder.gd
@@ -3,12 +3,23 @@ extends Area2D
 @onready var mainCharacter: CharacterBody2D = $"../mainCharacter"
 @onready var gameCamera: Camera2D = $"../mainCharacter/gameCamera"
 @onready var animatedSprite2D: AnimatedSprite2D = $AnimatedSprite2D
+@onready var topLeftMarker: Marker2D = $topLeft
+@onready var bottomRightMarker: Marker2D = $bottomRight
 
 var inRange = false
+var topLeft: Vector2
+var bottomRight: Vector2
+
+func _ready() -> void:
+	topLeft = topLeftMarker.get_global_position()
+	bottomRight = bottomRightMarker.get_global_position()
+	print(topLeft)
+	print(bottomRight)
 
 func _process(_delta: float) -> void:
 	if inRange && Input.is_action_just_pressed("Interact"):
 		mainCharacter.set_position($Destination.global_position)
+		fixCamera()
 
 func _on_body_entered(_body: Node2D) -> void:
 	animatedSprite2D.show()
@@ -18,8 +29,8 @@ func _on_body_exited(_body: Node2D) -> void:
 	animatedSprite2D.hide()
 	inRange = false
 
-func fixCamera(left, right, top, bottom):
-	gameCamera.limit_left = left
-	gameCamera.limit_right = right
-	gameCamera.limit_top = top
-	gameCamera.limit_bottom = bottom
+func fixCamera():
+	gameCamera.limit_top = int(topLeft.y)
+	gameCamera.limit_left = int(topLeft.x)
+	gameCamera.limit_bottom = int(bottomRight.y)
+	gameCamera.limit_right = int(bottomRight.x)
--- scripts/globalVars.gd
+++ scripts/globalVars.gd
@@ -1,4 +1,6 @@
 extends Node
 
-var filePath := "user://saveData.sav"
+var filePath := "user://mainSaveData.sav"
+var filePath2 := "user://enemySaveData.sav"
+
 var bytes := 30
