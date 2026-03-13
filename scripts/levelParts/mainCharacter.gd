9f26852778e74d26f97a997585b10f35c9cf2dc7
Nova <kt.drakon@gmail.com>
2026-03-12 22:58:29 -0400

    Version 3.1.8
    General Fixes

--- scripts/levelParts/mainCharacter.gd
+++ scripts/levelParts/mainCharacter.gd
@@ -2,28 +2,50 @@ extends CharacterBody2D
 
 @onready var animatedSprite2D: AnimatedSprite2D = $AnimatedSprite2D
 
-var speed := 200
-var jump := -300
+const speed := 180
+const jump := -300
+const gravity := 950
+const accelertation := 900
+const friction := 1000
 
-func _physics_process(delta: float) -> void:
-	var direction := Input.get_axis("Left", "Right")
-	
-	if direction > 0:
-		animatedSprite2D.flip_h = false
-	elif  direction < 0:
-		animatedSprite2D.flip_h = true
+const coyoteTime := 0.1
+var coyoteTimer := 0.0
+
+func handle_animation(direction: float) -> void:
+	if direction:
+		animatedSprite2D.flip_h = direction < 0
 	
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		animatedSprite2D.play("Jump")
-		velocity.y = jump
-		await get_tree().create_timer(0.65).timeout
+	
+	if is_on_floor():
 		animatedSprite2D.play("Idle")
 
-	if direction:
-		velocity.x = direction * speed
+func _physics_process(delta: float):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if is_on_floor():
+		coyoteTimer = coyoteTime
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		coyoteTimer -= delta
+
+	if Input.is_action_just_pressed("Jump"):
+		$jumpBuffer.start()
+	
+	if Input.is_action_just_pressed("Jump") and coyoteTimer > 0:
+		velocity.y = jump
+		coyoteTimer = 0
+	
+	if is_on_floor() and !$jumpBuffer.is_stopped():
+		velocity.y = jump
+	
+	var direction := Input.get_axis("Left", "Right")
+
+	if direction:
+		velocity.x = move_toward(velocity.x, direction * speed, accelertation * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, friction * delta)
+	
+	handle_animation(direction)
 	move_and_slide()
